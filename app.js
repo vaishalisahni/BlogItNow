16e9327665c48dea217af00e314ab2234b614d2a
Vaishali Sahni <vaishalisahni10@gmail.com>
2025-07-10 17:43:52 +0530

    d

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ app.get("/", async(req, res) => {
 app.use("/user",userRoute);
 app.use("/blog",BlogRoute)
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server started at Port:${PORT}`);
 });
